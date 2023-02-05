c18484ebd1707f99daacad797ed2d44c81ffc2cd
Asta Halkjær From <andro.from@gmail.com>
2023-02-05 16:46:06 +0100

    Add tool as subpage.

---
+++ cartouches/pkg/cartouches_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function extract_snippets(a: number, b: number, c: number, d: number, e: number): void;
+export function main(): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_start(): void;
